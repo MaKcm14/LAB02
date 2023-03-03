f4eee0291626076c3393f11f3f80d24255ba5ab1
MaKcm14 <maksimacx@icloud.com>
2023-03-03 19:51:13 +0300

    Был исправлен код в ветке patch1: добавлены некоторые пробелы и убрана директива using namespace std

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <string>
-using namespace std;
 
 int main() {
-  string name;
-  cout << "Hello! What's your name?";
+  std::string name;
+  std::cout << "Hello! What's your name?";
   getline(cin, name);
-  cout << "Hello, World from " << name;
+  std::cout << "Hello, World from " << name;
   return 0;
 }
