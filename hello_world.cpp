56831c6a7c3a2b2385c4c32d806cc23f9d57f7dc
MaKcm14 <maksimacx@icloud.com>
2023-03-03 20:05:05 +0300

    Были добавлены комментарии, поясняющие, что происходит в коде

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
+#include <iostream> 
 #include <string>
 
 int main() {
-  std::string name;
-  std::cout << "Hello! What's your name?";
-  getline(cin, name);
-  std::cout << "Hello, World from " << name;
+  std::string name; // создание переменной для хранения имени
+  std::cout << "Hello! What's your name?"; // спрашиваем пол-ля имя
+  getline(cin, name); // записываем имя из cin в name
+  std::cout << "Hello, World from " << name; // выводим последнее сооб.
   return 0;
 }
