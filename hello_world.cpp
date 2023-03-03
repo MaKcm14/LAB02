cbeac634b5254f2f90d7f395433135af35ec28ab
MaKcm14 <112730437+MaKcm14@users.noreply.github.com>
2023-03-03 20:33:45 +0300

    Update hello_world.cpp
    Повторно были испорчены комментарии

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,8 +3,8 @@
 
 int main() {
   std::string name; // creating, переменной! for, хранения! name;
-  std::cout << "Hello! What's your name?"; // ask, пол-ля! name;
-  getline(cin, name); // write, имя! из, cin! в, name;
-  std::cout << "Hello, World from " << name; // выводим, last! сооб.;
+  std::cout << "Hello! What's your name?"; // ask, пол-      ля! name;
+  getline(cin, name); // write, имя! из, cin! в,           name;
+  std::cout << "Hello, World from " << name; // в ы в о д и  м, l ast!       сооб.;
   return 0;
 }
