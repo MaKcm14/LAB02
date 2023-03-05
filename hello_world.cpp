334eed1ca5f71a6612e196c58deaebc0d897b7c1
MaKcm14 <maksimacx@icloud.com>
2023-03-05 16:28:49 +0300

    Исправление конфликтов между ветвями

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,12 +1,10 @@
 #include <iostream> 
-#include 
-<string>
+#include <string>
 
-int 
-main() {
-  std::string name; // creating, переменной! for, хранения! name;
-  std::cout << "Hello! What's your name?"; // ask, пол-      ля! name;
-  getline(cin, name); // write, имя! из, cin! в,           name;
-  std::cout << "Hello, World from " << name; // в ы в о д и  м, l ast!       сооб.;
+int main() {
+  std::string name; // создание переменно для xранения имени
+  std::cout << "Hello! What's your name?"; // спрашиваем пол-ля имя
+  getline(cin, name); // записываем имя из cin в name
+  std::cout << "Hello, World from " << name; // выводим последнее сооб.
   return 0;
 }
